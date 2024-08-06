f985e7a7337481dc43e85b19ee75b9dd80eae5ef
rlcosta177 <rlcosta177@gmail.com>
2024-08-06 10:39:54 +0000

    countdown timer. delete components folder in calculator

---
+++ timer/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CountdownTimer from './components/countdown_timer';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Timer</h1>
+      <CountdownTimer />
+    </div>
+  );
+};
+
+export default App;
