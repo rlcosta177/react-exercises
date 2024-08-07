e84c50b1fe3ab342d88cbe292cb5c8e3c1a73020
rlcosta177 <rlcosta177@gmail.com>
2024-08-07 13:29:03 +0000

    small change

--- carousel/src/App.js
+++ carousel/src/App.js
@@ -8,15 +8,15 @@ class carouselapp extends Component {
             <Carousel>
                 <div>
                     <img src="https://imgb.ifunny.co/images/59339ec63b3f151e62008b6b862f84e7e8a9b5efb027913eedf082dd26e7721c_1.jpg" />
-                    <p className="legend">Legend 1</p>
+                    <p className="legend">img 1</p>
                 </div>
                 <div>
                     <img src="https://external-preview.redd.it/skibidi-gyatt-ohio-rizzler-fanum-tax-v0-M3NhYXk0cGV2YzdjMbG8tBu7DkfNZFCfRsxbIKrLXiiLaLuoDqmbTSfNyngr.png?format=pjpg&auto=webp&s=7e25dd19ebd2fd9fd3eb3724ff1f9a015be257d7" />
-                    <p className="legend">Legend 2</p>
+                    <p className="legend">img 2</p>
                 </div>
                 <div>
                     <img src="https://i.pinimg.com/originals/3a/ba/b9/3abab9a2e8d1e2ae23e6d98d248be8da.jpg" />
-                    <p className="legend">Legend 3</p>
+                    <p className="legend">img 3</p>
                 </div>
             </Carousel>
         );
